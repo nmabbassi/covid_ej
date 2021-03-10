1e136348045bda9081e0d590b2bb4357e7f5c5b9
Nakiessa Abbassi <nabbassi@uoregon.edu>
2021-03-09 23:28:48 -0800

    .

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -13,9 +13,20 @@ source("useful_functions.R")
 #useful functions (fix later)
 library(ggplot2)
 library(readxl)
+library(tidyverse)
+library(dplyr)
+library(ggrepel)
 
-#load data
-covid_data <- read_excel("~/Desktop/covid_ej/Health_Atlas_Data.xlsx",
+#load covid data
+covid_data <- read_excel("input/Health_Atlas_Data.xlsx",
                       na="NA") 
 
 #clean covid data
+new_covid <- covid_data %>%
+             select(`geoid`, `covid_cases`, `covid_deaths`, 
+                     `covid_cases_percap`, `covid_deaths_percap`)
+  
+  covid_data %>%
+         rename(`census` = `geoid`,
+                )
+
--- analysis/useful_functions.R
+++ analysis/useful_functions.R
@@ -1,3 +1,17 @@
 ## useful_functions.R
 
 #Put any functions that might be used across multiple scripts here so that they can be properly sourced in.
+
+install.packages("tidyverse")
+install.packages("readxl")
+install.packages("ca")
+install.packages("forecast")
+install.packages("MLmetrics")
+
+#excel read, graphs
+library(ggplot2)
+library(readxl)
+
+#below used for forecasting counterfactual
+library(forecast)
+library(MLmetrics)
